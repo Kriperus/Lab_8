10de8931b17c963b2573d44a0dd615fa68e8c52a
Kriperus <krasnyhaleksandr123@gmail.com>
2026-07-01 15:45:07 +0500

    Update TaskFileManager.cs

--- Lab_8/Lab_8/TaskFileManager.cs
+++ Lab_8/Lab_8/TaskFileManager.cs
@@ -1,17 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 
 namespace TaskScheduler
 {
-    // Класс для работы с бинарным файлом
+    // Класс для работы с бинарным файлом (ручная сериализация)
     public static class TaskFileManager
     {
         private const string DataFile = "tasks.dat";
 
+        // Сохранение базы данных в бинарный файл
+        public static void SaveTasks(List<Task> tasks)
+        {
+            try
+            {
+                using (var stream = new FileStream(DataFile, FileMode.Create, FileAccess.Write))
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8))
+                {
+                    // Записываем количество задач
+                    writer.Write(tasks.Count);
+
+                    // Записываем каждую задачу
+                    foreach (var task in tasks)
+                    {
+                        writer.Write(task.Id);
+                        writer.Write(task.Title ?? string.Empty);
+                        writer.Write(task.Description ?? string.Empty);
+                        writer.Write(task.DueDate.ToBinary()); // DateTime в бинарный формат
+                        writer.Write(task.Priority);
+                        writer.Write(task.IsCompleted);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при сохранении файла: {ex.Message}");
+                throw;
+            }
+        }
+
         // Чтение базы данных из бинарного файла
-#pragma warning disable SYSLIB0011 // Отключаем предупреждение об устаревании BinaryFormatter
         public static List<Task> LoadTasks()
         {
             try
@@ -21,11 +50,30 @@ namespace TaskScheduler
                     return new List<Task>();
                 }
 
+                var tasks = new List<Task>();
+
                 using (var stream = new FileStream(DataFile, FileMode.Open, FileAccess.Read))
+                using (var reader = new BinaryReader(stream, Encoding.UTF8))
                 {
-                    var formatter = new BinaryFormatter();
-                    return (List<Task>)formatter.Deserialize(stream);
+                    // Читаем количество задач
+                    int count = reader.ReadInt32();
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        var task = new Task
+                        {
+                            Id = reader.ReadInt32(),
+                            Title = reader.ReadString(),
+                            Description = reader.ReadString(),
+                            DueDate = DateTime.FromBinary(reader.ReadInt64()),
+                            Priority = reader.ReadInt32(),
+                            IsCompleted = reader.ReadBoolean()
+                        };
+                        tasks.Add(task);
+                    }
                 }
+
+                return tasks;
             }
             catch (Exception ex)
             {
@@ -33,24 +81,6 @@ namespace TaskScheduler
                 return new List<Task>();
             }
         }
-
-        // Сохранение базы данных в бинарный файл
-        public static void SaveTasks(List<Task> tasks)
-        {
-            try
-            {
-                using (var stream = new FileStream(DataFile, FileMode.Create, FileAccess.Write))
-                {
-                    var formatter = new BinaryFormatter();
-                    formatter.Serialize(stream, tasks);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка при сохранении файла: {ex.Message}");
-            }
-        }
-#pragma warning restore SYSLIB0011 // Восстанавливаем предупреждение
 
         // Проверка существования файла
         public static bool FileExists()
