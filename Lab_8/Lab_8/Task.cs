b2537ec05c9a24b0a150795c5060850641af272c
Kriperus <krasnyhaleksandr123@gmail.com>
2026-07-02 06:51:32 +0500

    Update Task.cs

--- Lab_8/Lab_8/Task.cs
+++ Lab_8/Lab_8/Task.cs
@@ -3,7 +3,7 @@ using System.Runtime.Serialization;
 
 namespace TaskScheduler
 {
-    [Serializable] // Важно для бинарной сериализации
+    [Serializable]
     public class Task
     {
         // Свойства
